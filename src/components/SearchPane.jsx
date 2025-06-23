5e02e7dd88d8f52dc10a48a247ac6b08afa56beb
nobodyn0se <nobodyn0se@protonmail.ch>
2025-06-23 19:54:37 +0530

    Updated search pane with deletion functionality and handlers

--- src/components/SearchPane.jsx
+++ src/components/SearchPane.jsx
@@ -1,13 +1,50 @@
+import {useAtom} from "jotai";
+import {airportMarkerAtom} from "../state/atoms.jsx";
+import {MdDeleteForever} from "react-icons/md";
+
+async function mockAirportSearch() {
+    const coordinates = [{
+        name: 'Almaty', long: 77.043, lat: 43.354
+    }, {name: 'Tashkent', long: 69.281, lat: 41.258}, {name: 'Baku', long: 50.047, lat: 40.467}, {
+        name: 'Tehran',
+        long: 51.152198791503906,
+        lat: 35.416099548339844
+    }]
+
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve(coordinates);
+        }, 1000);
+    })
+}
+
 function SearchPane() {
+    const [, setAirportMarkers] = useAtom(airportMarkerAtom);
+
+    const handleSearch = async () => {
+        const results = await mockAirportSearch();
+        setAirportMarkers(results);
+    }
+
+    const handleDeleteAll = () => {
+        setAirportMarkers([]);
+    }
+
     return (
         <div className="mb-4">
-            <input
-                type="text"
-                placeholder="Enter airport code"
-                className="w-full p-2 border border-gray-300 rounded mb-2"
-            />
-            <button className="w-full bg-blue-500 text-white p-2 rounded hover:bg-blue-600">
-                Add Airport
+            <div className="flex w-full">
+                <input
+                    type="text"
+                    placeholder="Enter airport code"
+                    className="w-full p-2 border border-gray-300 rounded mb-2"
+                />
+                <div className="mb-2 flex items-center px-3 rounded cursor-pointer">
+                    <MdDeleteForever className="text-xl text-red-500" onClick={handleDeleteAll}/>
+                </div>
+            </div>
+            <button className="w-full bg-blue-500 text-white p-2 rounded hover:bg-blue-600"
+                    onClick={handleSearch}>
+                Add Route
             </button>
         </div>
     )
