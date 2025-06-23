6c971ded562ea0487ca483467dfcb5ce488950ae
nobodyn0se <nobodyn0se@protonmail.ch>
2025-06-23 11:51:30 +0530

    Created a loading spinner for map

---
+++ src/util/MapSpinner.jsx
@@ -0,0 +1,10 @@
+import {ImSpinner3} from "react-icons/im";
+import React from "react";
+
+function MapSpinner() {
+    return (
+        <div className="text-center p-4 h-full flex items-center justify-center bg-gray-800"><ImSpinner3/></div>
+    );
+}
+
+export default MapSpinner;
