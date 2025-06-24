37d6846e1cde2aeb7c492d2532ed8f6d7b7cc915
nobodyn0se <nobodyn0se@protonmail.ch>
2025-06-24 13:45:50 +0530

    Defined util methods for great circle computations

---
+++ src/util/util.jsx
@@ -0,0 +1,29 @@
+import * as turf from '@turf/turf';
+
+const generateGreatCircle = (start, end, numPoints = 4) => {
+    const startPoint = turf.point(start);  // Start coordinates [longitude, latitude]
+    const endPoint = turf.point(end);      // End coordinates [longitude, latitude]
+
+    const greatCircle = turf.greatCircle(startPoint, endPoint, {npoints: numPoints});
+
+    return greatCircle.geometry.coordinates;
+};
+
+const createGCPaths = (currentAirportMarkers) => {
+    let allCoordinates = [];
+
+    for (let i = 0; i < currentAirportMarkers.length - 1; i++) {
+        const start = [currentAirportMarkers[i].long, currentAirportMarkers[i].lat];
+        const end = [currentAirportMarkers[i + 1].long, currentAirportMarkers[i + 1].lat];
+
+        // Generate the great circle path between consecutive markers
+        const greatCircleCoordinates = generateGreatCircle(start, end);
+
+        // Append the great circle coordinates to the allCoordinates array
+        allCoordinates = allCoordinates.concat(greatCircleCoordinates);
+
+        return allCoordinates;
+    }
+}
+
+export {createGCPaths};
