a9835ae6117046db8677c3ef7d3ad8db46ca4909
nobodyn0se <nobodyn0se@protonmail.ch>
2025-06-30 10:44:39 +0530

    Added JSDoc comments for util methods

--- src/util/util.ts
+++ src/util/util.ts
@@ -1,8 +1,16 @@
 import * as turf from '@turf/turf';
 import {Airport, PolylineRoute} from "@customTypes/global.types.ts";
-import {GCCoordinates} from "@customTypes/util.types.ts";
+import {Coordinate, GCCoordinates} from "@customTypes/util.types.ts";
+import {Feature, Polygon} from "geojson";
 
-const generateGreatCircle = (start: [number, number], end: [number, number], numPoints = 10): [number, number][] => {
+/**
+ * Create a 1-level nested array of [numPoints] coordinates between start and end having format Coordinate
+ * @param start
+ * @param end
+ * @param numPoints
+ * @returns {Coordinate[]} coordinates of the great distance of type {@link Coordinate}
+ */
+const generateGreatCircle = (start: Coordinate, end: Coordinate, numPoints = 10): Coordinate[] => {
     const startPoint = turf.point(start);  // Start coordinates [longitude, latitude]
     const endPoint = turf.point(end);      // End coordinates [longitude, latitude]
 
@@ -10,18 +18,23 @@ const generateGreatCircle = (start: [number, number], end: [number, number], num
     const gcCoordinates = greatCircle.geometry.coordinates as GCCoordinates;
 
     if (Array.isArray(gcCoordinates[0]) && Array.isArray(gcCoordinates[0][0])) {
-        return gcCoordinates.flat() as [number, number][];
+        return gcCoordinates.flat() as Coordinate[];
     }
-    return gcCoordinates as [number, number][];
+    return gcCoordinates as Coordinate[];
 };
 
-const createGCPaths = (currentAirportMarkers: Airport[]) => {
-    let allCoordinates: [number, number][] = [];
+/**
+ * Creates a custom object containing the great circle path for a given route with coordinates and distance for each subroute
+ * @param currentAirportMarkers
+ * @returns {coordinates: Coordinate[], distance: number[]} object containing a list of coordinates and corresponding great circle distance
+ */
+const createGCPaths = (currentAirportMarkers: Airport[]): { coordinates: Coordinate[], distance: number[] } => {
+    let allCoordinates: Coordinate[] = [];
     let gcDistance: number[] = [];
 
     for (let i = 0; i < currentAirportMarkers.length - 1; ++i) {
-        const start: [number, number] = [currentAirportMarkers[i].long, currentAirportMarkers[i].lat];
-        const end: [number, number] = [currentAirportMarkers[i + 1].long, currentAirportMarkers[i + 1].lat];
+        const start: Coordinate = [currentAirportMarkers[i].long, currentAirportMarkers[i].lat];
+        const end: Coordinate = [currentAirportMarkers[i + 1].long, currentAirportMarkers[i + 1].lat];
 
         gcDistance.push(turf.distance(turf.point(start), turf.point(end)));
 
@@ -35,7 +48,14 @@ const createGCPaths = (currentAirportMarkers: Airport[]) => {
     return {coordinates: allCoordinates, distance: gcDistance};
 }
 
-const createPolarCircle = (lat: number, radiusKm: number, points = 64) => {
+/**
+ * Create a circle of [radiumKm] kilometers with center at (0, lat)
+ * @param lat
+ * @param radiusKm
+ * @param points
+ * @returns {Feature<Polygon>} a circle polygon with provided center and radius
+ */
+const createPolarCircle = (lat: number, radiusKm: number, points = 64): Feature<Polygon> => {
     return turf.circle([0, lat], radiusKm, {
         steps: points,
         units: 'kilometers'
@@ -47,6 +67,11 @@ const polarRadiusKm = 1100;
 const arcticCircle = createPolarCircle(89.9, polarRadiusKm);     // Arctic circle
 const antarcticCircle = createPolarCircle(-89.9, polarRadiusKm);      // Antarctic circle
 
+/**
+ * Creates a polyline path for a given route with added properties like name of the route and IATA list of airports in the route
+ * @param currentAirportMarkers
+ * @returns {PolylineRoute} custom polyline object containing route data
+ */
 function createNewPolylineRoute(currentAirportMarkers: Airport[]): PolylineRoute {
     const routePath = createGCPaths(currentAirportMarkers);
     const airportsIATAList = currentAirportMarkers.map(airport => airport.iata);
