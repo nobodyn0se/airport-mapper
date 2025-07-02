f194d5d07061537556f6179ef609414a8d1e095b
nobodyn0se <nobodyn0se@protonmail.ch>
2025-07-02 11:35:57 +0530

    New atom added to track IATA for marker deletion

--- src/state/atoms.ts
+++ src/state/atoms.ts
@@ -4,3 +4,4 @@ import {Airport, PolylineRoute} from "@customTypes/global.types.ts";
 export const airportMarkerAtom = atom<Airport[]>([])
 export const currentAirportMarkerAtom = atom<Airport[]>([])
 export const polylinesAtom = atom<PolylineRoute[]>([])
+export const markerDeletionAtom = atom<string>('')
