c9b30ba013c52ba32d205593b0a3126d9264286a
nobodyn0se <nobodyn0se@protonmail.ch>
2025-07-04 20:34:12 +0530

    Added an FSA based state to manage lookup feedback effectively

--- src/types/util.types.ts
+++ src/types/util.types.ts
@@ -1,2 +1,10 @@
 export type GCCoordinates = [number, number][] | [number, number][][];
 export type Coordinate = [number, number];
+
+export enum LookupState {
+    IDLE = 'IDLE',
+    LOADING = 'LOADING',
+    ERROR = 'ERROR',
+    HASDATA = 'HASDATA',
+    NODATA = 'NODATA',
+}
