32b80a61ea9e1b6a79bab8e11a58dcb5adaacfb9
nobodyn0se <nobodyn0se@protonmail.ch>
2025-07-04 23:53:31 +0530

    Adjusted route list height to be scrollable

--- src/components/RouteList.tsx
+++ src/components/RouteList.tsx
@@ -11,7 +11,7 @@ function RouteList() {
     const totalAllRoutes = calculateTotalDistance(polylines.map(polyline => polyline.totalRouteDistance));
 
     return (
-        <div>
+        <div className="h-5/6">
             <div className="mb-4 p-4 border border-gray-300 rounded">
                 <h4 className="font-semibold">Total Distance: {totalAllRoutes} km</h4>
                 <ul className="list-disc pl-5">
@@ -19,34 +19,36 @@ function RouteList() {
                 </ul>
             </div>
             {/* Add more route placeholders as needed */}
-            {polylines && polylines.map((route) => (
-                <ul className="max-w-md mx-auto rounded-lg overflow-hidden divide-y divide-gray-200">
-                    <li key={route.name} className="px-4 py-3">
-                        <div className="flex justify-between items-center">
-                            <div className="flex flex-col space-x-2">
-                                {route.airports.slice(0, -1).map((airport, i) => (
-                                    <div key={`${airport}-${i}`} className="flex items-center">
-                                        <span className="font-medium text-white-700">{airport}</span>
-                                        <span className="mx-1 text-gray-500">→</span>
-                                        <span className="font-medium text-white-700">{route.airports[i + 1]}</span>
-                                    </div>
-                                ))}
-                            </div>
+            <section className="h-5/6 overflow-y-auto divide-y divide-gray-500">
+                {polylines && polylines.map((route) => (
+                    <ul className="max-w-md mx-auto my-2 rounded-lg overflow-hidden">
+                        <li key={route.name} className="px-4 py-3">
+                            <div className="flex justify-between items-center">
+                                <div className="flex flex-col space-x-2">
+                                    {route.airports.slice(0, -1).map((airport, i) => (
+                                        <div key={`${airport}-${i}`} className="flex items-center">
+                                            <span className="font-medium text-white-700">{airport}</span>
+                                            <span className="mx-1 text-gray-500">→</span>
+                                            <span className="font-medium text-white-700">{route.airports[i + 1]}</span>
+                                        </div>
+                                    ))}
+                                </div>
 
-                            <aside className="flex flex-col">
-                                {route.distance.map((distance, i) => (
-                                    <span
-                                        key={i}
-                                        className="font-semibold text-right"
-                                    >
+                                <aside className="flex flex-col">
+                                    {route.distance.map((distance, i) => (
+                                        <span
+                                            key={i}
+                                            className="font-semibold text-right"
+                                        >
                                     {distance.toFixed(2)}
                                     </span>
-                                ))}
-                            </aside>
-                        </div>
-                    </li>
-                </ul>
-            ))}
+                                    ))}
+                                </aside>
+                            </div>
+                        </li>
+                    </ul>
+                ))}
+            </section>
         </div>
     )
 }
